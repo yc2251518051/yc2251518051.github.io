9af80125f25f893cf8e36442e44931346d2cd328
yangzhicheng <62593908+yc2251518051@users.noreply.github.com>
2020-08-03 20:15:43 +0800

    添加了首页推荐信息

--- vueProtect/supermarket/src/views/home/home.vue
+++ vueProtect/supermarket/src/views/home/home.vue
@@ -5,28 +5,32 @@
     </navBar>
     <swiper :banners=banners>
     </swiper>
+    <recommend :recommends="recommend"></recommend>
   </div>
 </template>
 <script>
 import navBar from "@/components/common/navbar/navBar";
 import { getHomeMultidata } from "@/network/home.js";
-import swiper from "@/components/common/swiper/swiper"
-import swiperItem from "@/components/common/swiper/swiperItem"
+import swiper from "@/components/common/swiper/swiper";
+import recommend from "@/components/content/home/HomeRecommendViews";
 export default {
   name: "home",
   data() {
     return {
-      banners: []
+      banners: [],
+      recommend:[]
     };
   },
   components: {
     navBar,
     swiper,
-    swiperItem
+    recommend
   },
   created() {
     //1.请求多个数据
     getHomeMultidata().then(res => {
+      console.log(res)
+      this.recommend = res.data.recommend.list;
       this.banners = res.data.banner.list;
     });
   }
